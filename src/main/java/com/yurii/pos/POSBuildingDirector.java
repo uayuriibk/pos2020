a63f2fbee551605851c4c36eb46e94e1bb279e0b
yuriybk <email2yurii@gmail.com>
2020-07-14 03:22:13 +0300

    fix for Building Director

--- src/main/java/com/yurii/pos/POSBuildingDirector.java
+++ src/main/java/com/yurii/pos/POSBuildingDirector.java
@@ -7,6 +7,10 @@ public class POSBuildingDirector {
     @Getter @Setter
     private POSAbstractBuilder posAbstractBuilder;
 
+    public POS getPOS(){
+        return posAbstractBuilder.getPos();
+    }
+
     public void constructPOS(){
         posAbstractBuilder.createNewPOS();
         posAbstractBuilder.buildScanner();
