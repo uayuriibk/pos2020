6bf4abf6cd200cd3167820682b3b33ca8d258d83
yuriybk <email2yurii@gmail.com>
2020-07-18 06:01:35 +0300

    interface for scanner has been edited

--- src/main/java/com/yurii/pos/devices/IScanner.java
+++ src/main/java/com/yurii/pos/devices/IScanner.java
@@ -2,6 +2,7 @@ package com.yurii.pos.devices;
 
 import com.yurii.pos.exceptions.InvalidBarCodeException;
 import com.yurii.pos.exceptions.ProductNotFoundException;
+import com.yurii.pos.products.IProductsInfoDB;
 
 import java.math.BigDecimal;
 import java.util.Map;
@@ -10,5 +11,6 @@ public interface IScanner {
     void scanCode(String code) throws ProductNotFoundException, InvalidBarCodeException;
     Map<String, String> getReceipt();
     BigDecimal calculateTotalPrice();
+    void setProductsInfoDB(IProductsInfoDB productsData);
     void clearScanned();
 }
